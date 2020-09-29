f34ce62dadabe47cba5d3a6b358012515071b09c
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-29 07:49:49 +0200

    Added SamuraiContext

---
+++ SamuraiApp.Data/Models/SamuraiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain.DataModels;
+
+namespace SamuraiApp.Data.Models
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+    }
+}
