14c0d67ed7b9990543e87fabce09c6b3fa8c5eac
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-29 08:34:36 +0200

    Add demo ConsoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using SamuraiApp.Data.Models;
+using SamuraiApp.Domain.DataModels;
+using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+
+        private static SamuraiContext samuraiContext = new SamuraiContext();
+
+        static void Main(string[] args)
+        {
+            samuraiContext.Database.EnsureCreated();
+            GetSamurais("Before Add");
+            AddSamurai();
+            GetSamurais("After Add");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Luci" };
+            samuraiContext.Samurais.Add(samurai);
+            samuraiContext.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = samuraiContext.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
