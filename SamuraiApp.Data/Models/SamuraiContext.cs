397ac32c1cafe140c565a91f440e870ded749317
Borcze Angelov <borcze.angelov@yahoo.com>
2020-09-29 08:00:48 +0200

    Set Data source to local DB

--- SamuraiApp.Data/Models/SamuraiContext.cs
+++ SamuraiApp.Data/Models/SamuraiContext.cs
@@ -8,5 +8,11 @@ namespace SamuraiApp.Data.Models
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
